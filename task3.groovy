5ba9815318c726fd0c3f2cfe7f8becfbc11d0b9e
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-04-19 01:16:54 +0200

    set 3 almost done.

--- task3.groovy
+++ task3.groovy
@@ -7,11 +7,24 @@ g.V().has('airport','country','PL').outE().has('dist',gt(400)).inV() \
      .has('country', 'DE').path().by('code').by('dist')
 
 // Task 3.
-g.V().has('airport', 'code', 'MUC').inE().outV().has('country', 'PL').as('pl').outE().as('dist').inV().has('country', 'DE').as('de').path().select('pl', 'de', 'dist').by('code').by('code').by('dist')
+g.V().has('airport', 'code', 'MUC').inE().outV().has('country', 'PL') \
+     .as('pl').outE().as('dist').inV().has('country', 'DE').as('de')  \
+     .path().select('pl', 'de', 'dist').by('code').by('code').by('dist')
 
+// Task 4.
+g.V().has('code', 'WRO').repeat(out()).emit(has('airport', 'country', 'UK')) \
+     .times(3).out().has('airport', 'country', 'UK').path().by('code')
 
-// Task 6.
+// Task 5.
+g.V().has('code', 'WRO').repeat(out().has('airport', 'country', 'UK')).emit() \
+     .times(4).path().by('code')
 
+// Task 6. Not finished
+
+g.V().has('code', 'WRO').repeat(out().simplePath()).emit(has('airport', 'country', 'UK')).until(out().has('airport', 'country', 'UK')).out().has('airport', 'country', 'UK').as('out').select('out').by('code').dedup().fold()
+
+g.V().has('code', 'WRO').repeat(out()).emit().times(3).out().has('airport', 'country', 'UK').as('out').select('out').by('code').dedup().fold().toList()
+// g.V().has('code', 'WRO').repeat(out()).emit().times(3).out().has('airport', 'country', 'UK').path().by('code')
 
 // Task 7.
 g.V().has('country', 'PL')                       \
@@ -30,3 +43,8 @@ g.V().has('country', 'PL')                       \
      .select('airport','countries')              \
      .by('code')                                 \
      .by(out().values('country').dedup().fold())
+
+// Task 9.
+g.V().has('continent','code','EU').out().has('type', 'airport') \
+      .order().by(outE('route').count(), decr).limit(10)        \
+      .project('code','city','cnt').by('code').by('city').by(outE('route').count())
