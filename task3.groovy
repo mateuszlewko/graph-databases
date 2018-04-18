8864330307639bb05fa03840a1b03a50426ef293
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-04-18 23:59:00 +0200

    Done up to task 3.

--- task3.groovy
+++ task3.groovy
@@ -1,3 +1,28 @@
+// Task 1.
+g.V().has('airport','country','PL').outE().inV() \
+     .has('country', 'DE').path().by('code').by('dist')
+
+// Task 2.
+g.V().has('airport','country','PL').outE().has('dist',gt(400)).inV() \
+     .has('country', 'DE').path().by('code').by('dist')
+
+// Task 3.
+g.V().has('airport', 'code', 'MUC').inE().outV().has('country', 'PL').as('pl').outE().as('dist').inV().has('country', 'DE').as('de').path().select('pl', 'de', 'dist').by('code').by('code').by('dist')
+
+
+// Task 6.
+
+
+// Task 7.
+g.V().has('country', 'PL')                       \
+     .has('type','airport')                      \
+     .as('airport','inCnt', 'outCnt')            \
+     .select('airport','inCnt', 'outCnt')        \
+     .by('code')                                 \
+     .by(inE('route').count())                   \
+     .by(outE('route').count())                  
+
+
 // Task 8.
 g.V().has('country', 'PL')                       \
      .has('type','airport')                      \
